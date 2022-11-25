cca63e4ec7895354cbe9cd0bdff671e01a689888
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-11-25 22:58:53 +0600

    done modification

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -22,19 +22,21 @@ const Home = () => {
     <div>
       <Slider></Slider>
 
-      <div className="flex flex-col justify-center items-center mb-10 bg-slate-700 p-4 lg:p-10">
+      <div className="mb-10 bg-slate-700 p-4 ">
         <h2 className="text-xl lg:text-2xl uppercase my-3 text-center">
           Second Hand Laptop Categories
         </h2>
-        {categories.map((category) => (
-          <Link
-            to={`/category/${category._id}`}
-            key={category._id}
-            className="btn btn-accent rounded-none text-lg uppercase font-semibold w-4/5 lg:w-5/12 my-2"
-          >
-            {category.name}
-          </Link>
-        ))}
+        <div className="grid lg:grid-cols-3 gap-5">
+          {categories.map((category) => (
+            <Link
+              to={`/category/${category._id}`}
+              key={category._id}
+              className="btn btn-accent btn-outline rounded-none text-lg w-full uppercase font-semibold my-2"
+            >
+              {category.name}
+            </Link>
+          ))}
+        </div>
       </div>
 
       <ExtraSection></ExtraSection>
