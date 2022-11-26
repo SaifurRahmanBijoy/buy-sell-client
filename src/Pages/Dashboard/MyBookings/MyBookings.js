f81a4f97ae9c8560d3b2f18227c83b01bf850a56
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-11-26 22:07:44 +0600

    done 404 and error element

--- src/Pages/Dashboard/MyBookings/MyBookings.js
+++ src/Pages/Dashboard/MyBookings/MyBookings.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React, { useContext } from "react";
+import { AuthContext } from "../../../contexts/AuthProvider/AuthProvider";
 
 const MyBookings = () => {
-    return (
-        <div>
-            <h2>MyBookings</h2>
-        </div>
-    );
+  const { user } = useContext(AuthContext);
+  const { email } = user;
+  const { data: bookings = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:5000/bookings/${email}`);
+      const data = await res.json();
+      return data;
+    },
+  });
+  return (
+    <div className="overflow-x-auto text-sm p-4">
+      <table className="table w-full">
+        <thead className="uppercase bg-slate-400">
+          <tr>
+            <th>Product Picture</th>
+            <th>Product Name</th>
+            <th>Price</th>
+            <th>Pay</th>
+          </tr>
+        </thead>
+        <tbody>
+          {bookings.map((b) => (
+            <tr key={b._id} className="border border-slate-700 my-2">
+              <td><img className="w-32" src={b.productImg} alt="" /></td>
+              <td>{b.productName ? b.productName : "Unknown"}</td>
+              <td>${b.price}</td>
+
+              <td>
+                <button
+                  //   onClick={() => handleDelete(b._id)}
+                  className="btn rounded-sm btn-sm btn-accent text-yellow-50"
+                >
+                  Pay
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default MyBookings;
