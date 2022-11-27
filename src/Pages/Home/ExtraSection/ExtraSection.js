f34b4d813bb9435a4e168734a5a89710520c7a78
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-11-27 22:48:46 +0600

    done

--- src/Pages/Home/ExtraSection/ExtraSection.js
+++ src/Pages/Home/ExtraSection/ExtraSection.js
@@ -1,7 +1,14 @@
 import React from "react";
-import svg from './undraw_join_re_w1lh.svg'
+import toast from "react-hot-toast";
+import svg from "./undraw_join_re_w1lh.svg";
 
 const ExtraSection = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    toast.success(`Thank you for supporting us! Dear ${email}`);
+    event.target.reset();
+  };
   return (
     <div className="bg-gray-900 my-5">
       <div className="container px-6 py-16 mx-auto">
@@ -9,8 +16,7 @@ const ExtraSection = () => {
           <div className="w-full lg:w-1/2">
             <div className="lg:max-w-lg">
               <h1 className="text-2xl font-semibold text-gray-800 dark:text-white lg:text-3xl">
-                Subscribe To{" "}
-                <span className="text-blue-500">Wish Boat</span>
+                Subscribe To <span className="text-blue-500">Wish Boat</span>
               </h1>
 
               <p className="mt-4 text-gray-600 dark:text-gray-400">
@@ -20,16 +26,18 @@ const ExtraSection = () => {
               </p>
 
               <div className="flex flex-col mt-8 space-y-3 lg:space-y-0 lg:flex-row">
-                <input
-                  id="email"
-                  type="text"
-                  className="px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
-                  placeholder="Email Address"
-                />
+                <form onSubmit={handleSubmit}>
+                  <input
+                    id="email"
+                    type="text"
+                    className="px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
+                    placeholder="Email Address"
+                  />
 
-                <button className="w-full px-6 py-2.5 text-sm font-medium tracking-wider text-white uppercase transition-colors duration-300 transform bg-blue-600 rounded-lg lg:w-auto lg:mx-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
-                  Subscribe
-                </button>
+                  <button className="w-full px-6 py-2.5 text-sm font-medium tracking-wider text-white uppercase transition-colors duration-300 transform bg-blue-600 rounded-lg lg:w-auto lg:mx-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                    Subscribe
+                  </button>
+                </form>
               </div>
             </div>
           </div>
