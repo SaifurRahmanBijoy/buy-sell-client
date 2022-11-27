ed5c62b6f48b6b9b52090cede2de544fbc943b1f
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-11-27 16:26:07 +0600

    done product deletion

---
+++ src/Pages/Home/AdvertisedItems/AdvertisedItems.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AdvertisedItems = ({ item }) => {
+  const { img, name } = item;
+  return (
+    <div className="border border-slate-500 p-1 rounded bg-base-300">
+      <img className="w-full rounded shadow-lg " src={img} alt="" />
+      <h2 className="text-center text-md">{name}</h2>
+    </div>
+  );
+};
+
+export default AdvertisedItems;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { Link } from "react-router-dom";
 import Loading from "../../Shared/Loading/Loading";
+import AdvertisedItems from "../AdvertisedItems/AdvertisedItems";
 import ExtraSection from "../ExtraSection/ExtraSection";
 import Slider from "../Slider/Slider";
 
@@ -15,12 +16,36 @@ const Home = () => {
     },
   });
 
+  const { data: advertisedItems = [] } = useQuery({
+    queryKey: ["advertiseditems"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/advertiseditems");
+      const data = await res.json();
+      return data;
+    },
+  });
+
   if (isLoading) {
     return <Loading></Loading>;
   }
   return (
     <div>
       <Slider></Slider>
+
+      {advertisedItems && (
+        <div className=" p-5 lg:p-10 lg:w-3/5 mx-auto bg-base-200 my-10 shadow-xl rounded-xl">
+          <h2 className="text-xl lg:text-3xl text-center font-semibold my-4">
+            Advertised Products
+          </h2>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+            {advertisedItems
+              .filter((i) => !i.paid)
+              .map((item, i) => (
+                <AdvertisedItems key={i} item={item}></AdvertisedItems>
+              ))}
+          </div>
+        </div>
+      )}
 
       <div className="mb-10 bg-slate-700 p-4 ">
         <h2 className="text-xl lg:text-2xl uppercase my-3 text-center">
