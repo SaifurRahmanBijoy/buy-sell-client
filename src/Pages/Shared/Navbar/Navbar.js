6409233c7fdd7f8b8655221499b533ea72d8fc3d
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-11-28 19:07:18 +0600

    finished loading

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -37,17 +37,16 @@ const Navbar = () => {
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
               fill="none"
               viewBox="0 0 24 24"
-              stroke="currentColor"
+              className="inline-block w-5 h-5 stroke-current"
             >
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
             </svg>
           </label>
           <ul
@@ -75,7 +74,19 @@ const Navbar = () => {
           tabIndex={2}
           className="btn btn-ghost lg:hidden"
         >
-          ⬇
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block w-5 h-5 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
         </label>
       </div>
     </div>
